6cffab5ec8b37689e38510db730cede468692652
Maciej Baranowski <maciekbaranowski@onet.pl>
2018-01-30 17:48:55 +0100

    Simple voting algorithm - working for basic testcase, probably not for complicated ones

--- algorithm.h
+++ algorithm.h
@@ -16,9 +16,46 @@ public:
       auto liczbaKandydatowB = io.readSingle();
       auto glosPerPosel = io.readLines(liczbaPoslow,4);
 
+      vector<int> glosPerKandydatA(liczbaKandydatowA);
+      vector<int> glosPerKandydatB(liczbaKandydatowB);
+      for (auto glos : glosPerPosel)
+      {
+        if(glos[0] == 1)
+        {
+          glosPerKandydatA[glos[1]]++;
+        }
+        else
+        {
+          glosPerKandydatB[glos[1]]++;
+        }
+        if(glos[2] == 1)
+        {
+          glosPerKandydatA[glos[3]]--;
+        }
+        else
+        {
+          glosPerKandydatB[glos[3]]--;
+        }
+
+      }
+
       vector<int> wybraniA;
       vector<int> wybraniB;
 
+      for (unsigned i = 0; i < liczbaKandydatowA; i++)
+      {
+        if (glosPerKandydatA[i] > 0)
+        {
+          wybraniA.push_back(i);
+        }
+      }
+      for (unsigned i = 0; i < liczbaKandydatowB; i++)
+      {
+        if (glosPerKandydatB[i] > 0)
+        {
+          wybraniB.push_back(i);
+        }
+      }
       io.writeSingle(wybraniA.size());
       io.os << endl;
       io.writeLine(wybraniA);
