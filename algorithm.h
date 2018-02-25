a4aa1379fbd3db19ba889b352e5684a495a73816
Maciej Baranowski <maciekbaranowski@onet.pl>
2018-02-25 13:53:21 +0100

    Version at the end of contest (may not work)

--- algorithm.h
+++ algorithm.h
@@ -17,41 +17,76 @@ private:
     auto T = io.readSingle();
     while (T-->0)
     {
+      auto XMAX = io.readSingle();
+      auto YMAX = io.readSingle();
       int odzywienie = 1;
       int X = 1;
-      int Y = 1;
+      int Y = YMAX;
       io.writeSingle(X);
       io.writeSingle(Y);
       io << "\n";
-      auto XMAX = io.readSingle();
-      auto YMAX = io.readSingle();
       auto paleta = io.readLines<int>(YMAX,XMAX);
 
-      while (true)
+      auto ruchE = [&] () {
+        if (X != XMAX and  paleta[Y-1][X] != 0 and paleta[Y-1][X] > 0 )
+        {
+          odzywienie = odzywienie + (odzywienie % paleta[Y-1][X-1]);
+          paleta[Y-1][X-1] = 0;
+          X++; io << ('E');
+          return true;
+        }
+        return false;
+      };
+      auto ruchW = [&] () {
+        if (X != 1 and  paleta[Y-1][X-2] != 0 and paleta[Y-1][X-2] > 0 )
+        {
+          odzywienie = odzywienie + (odzywienie % paleta[Y-1][X-1]);
+          paleta[Y-1][X-1] = 0;
+          X--; io << ('W');
+          return true;
+        }
+        return false;
+      };
+
+      auto ruchS = [&] () {
+        if (Y != YMAX and  paleta[Y][X-1] != 0 and paleta[Y][X-1] > 0 )
+        {
+          odzywienie = odzywienie + (odzywienie % paleta[Y-1][X-1]);
+          paleta[Y-1][X-1] = 0;
+          Y++; io << ('S');
+          return true;
+        }
+        return false;
+      };
+      auto ruchN = [&] () {
+        if (Y != 1 and  paleta[Y-2][X-1] != 0 and paleta[Y-2][X-1] > 0 )
+        {
+          odzywienie = odzywienie + (odzywienie % paleta[Y-1][X-1]);
+          paleta[Y-1][X-1] = 0;
+          Y--; io << ('N');
+          return true;
+        }
+        return false;
+      };
+      int licznik = XMAX*YMAX-1;
+      while (licznik>0)
       {
         for (int i = 1; i < XMAX; i++)
         {
-          odzywienie = odzywienie + (odzywienie % paleta[Y-1][X-1]);
-          X++; io << ('E');
+          ruchE(); licznik--;
         }
-        odzywienie = odzywienie + (odzywienie % paleta[Y-1][X-1]);
-        if (Y == YMAX)
-          break;
-        Y++; io << ('S');
+        ruchN(); licznik--;
         for (int i = 1; i < XMAX; i++)
         {
-          odzywienie = odzywienie + (odzywienie % paleta[Y-1][X-1]);
-          X--; io << ('W');
+          ruchW(); licznik--;
         }
-        odzywienie = odzywienie + (odzywienie % paleta[Y-1][X-1]);
-        if (Y == YMAX)
-          break;
-        Y++; io << ('S');
+        ruchN(); licznik--;
 
       }
       io << "\n";
       io.writeSingle(odzywienie);
       io << "\n";
+
     }
   }
 
